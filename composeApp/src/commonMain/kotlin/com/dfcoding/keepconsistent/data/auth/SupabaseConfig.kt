22c14f0f2c35d508db91191692fb6cd59ad6d16c
Diogo Ferreira <diogofer123@gmail.com>
2026-08-26 14:55:24 +0100

    Removed Supabase Config

--- composeApp/src/commonMain/kotlin/com/dfcoding/keepconsistent/data/auth/SupabaseConfig.kt
+++ composeApp/src/commonMain/kotlin/com/dfcoding/keepconsistent/data/auth/SupabaseConfig.kt
@@ -1,8 +1,5 @@
 package com.dfcoding.keepconsistent.data.auth
 
 object SupabaseConfig {
-    const val URL = "https://osnaiasivslwmtczousq.supabase.co" //supabase url project
-    const val ANON_KEY = "sb_publishable_HO0NLHyqcbU4ZxOrzoo7gA_pF3bA2SJ" //non public key from supabase
-
-    const val GOOGLE_WEB_CLIENT_ID = "1070563302525-fgksarvqec0v7bv45fj7e3t5dbn5bu3n.apps.googleusercontent.com" //clound console id
+    
 }
